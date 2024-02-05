d5283ebf452065d6743eacd10a66791a241f0bef
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2024-02-05 08:57:23 +0100

    sidebar

--- client/src/components/leftmenu/leftmenu.jsx
+++ client/src/components/leftmenu/leftmenu.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { FiHome } from "react-icons/fi";
 import { IoSearch } from "react-icons/io5";
 import { BiLibrary } from "react-icons/bi";
+import { GoPlusCircle } from "react-icons/go";
+import musicPlayer from '../../assets/placeholder.png'
 
 const Leftmenu = () => {
   return (
@@ -12,10 +14,19 @@ const Leftmenu = () => {
         <li><Link to="/" className={styles.link}><IoSearch className={styles.icon}/> Search</Link></li>
         <li><Link to="/" className={styles.link}><BiLibrary className={styles.icon}/> Your Library</Link></li>
         <br />
-        <li><Link to="/" className={styles.link}>Create Playlist</Link></li>
-        <li><Link to="/" className={styles.link}>Liked Songs</Link></li>
+        <li><Link to="/" className={styles.link}><GoPlusCircle className={styles.icon}/> Create Playlist</Link></li>
+        <li><Link to="/" className={styles.link}><GoPlusCircle className={styles.icon}/> Liked Songs</Link></li>
       </ul>
       <span></span>
+      <div className={styles.playlist}>
+        <p>Playlist</p>
+        <p>Playlist</p>
+        <p>Playlist</p>
+        <p>Playlist</p>
+      </div>
+      <div className={styles.image}>
+        <img src={musicPlayer} alt="Music" />
+      </div>
     </div>
   )
 }
